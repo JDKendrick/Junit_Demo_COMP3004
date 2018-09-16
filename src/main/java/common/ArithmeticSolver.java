8723f42df4e9276bfe8b3bc708d246ed9d37be3a
Jake Kendrick <43183524+JDKendrick@users.noreply.github.com>
2018-09-16 13:00:51 -0400

    Function: added the "solve" function to the "ArithmeticSolver" class

--- src/main/java/common/ArithmeticSolver.java
+++ src/main/java/common/ArithmeticSolver.java
@@ -23,5 +23,39 @@ public class ArithmeticSolver {
 	public double modulus(double leftOperand, double rightOperand) {
 		return leftOperand % rightOperand;
 	}
+
+	public double solve(String operand, Double leftOperand, Double rightOperand) {
+		
+		double answer = 0.0;
+		
+		switch(operand) {
+		
+		case "+" :
+			answer = add(leftOperand, rightOperand);
+			break;
+			
+		case "-" :
+			answer = subtract(leftOperand, rightOperand);
+			break;
+			
+		case "*" :
+			answer = multiply(leftOperand, rightOperand);
+			break;
+			
+		case "/" :
+			answer = divide(leftOperand, rightOperand);
+			break;
+			
+		case "%" :
+			answer = modulus(leftOperand, rightOperand);
+			break;
+			
+		default:
+			break;		
+		}
+		
+		return answer;
+		
+	}
 	
 }
